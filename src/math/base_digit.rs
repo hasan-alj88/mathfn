8304455b9353a863a7aa239d6bb70d9860c37bb1
hasan aljamea <hasan@bigaicompany.com>
2026-08-12 12:30:42 +0300

    feat: add minimal BaseDigit skeleton

---
+++ src/math/base_digit.rs
@@ -0,0 +1,6 @@
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct BaseDigit<const BASE: u128 = 256>;
+
+impl<const BASE: u128> BaseDigit<BASE> {
+    // Add your fields, storage enum, and methods here
+}
